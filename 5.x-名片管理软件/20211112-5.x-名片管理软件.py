75672bb3d6be6aa6ab5bc71f0431a8b9e3e2875e
XanderLee <xandector@gmail.com>
2021-11-12 15:30:21 +0800

    名片管理软件函数定义

---
+++ 5.x-名片管理软件/20211112-5.x-名片管理软件.py
@@ -0,0 +1,30 @@
+from defx_CardSystemFunctions_20211112 import *
+
+# 初始化名片列表
+aCardList = initCard()
+while True:
+    aSelect = input("Please select(1-6):")
+    if aSelect == 1:
+        # 添加
+        pass
+    elif aSelect == 2:
+        # 删除
+        pass
+    elif aSelect == 3:
+        # 修改
+        pass
+    elif aSelect == 4:
+        # 查询
+        pass
+    elif aSelect == 5:
+        # 一览
+        pass
+    elif aSelect == 6:
+        # 推出
+        pass
+    else:
+        print("输入有误，请重新输入：")
+        continue
+
+
+
---
+++ 5.x-名片管理软件/defx_CardSystemFunctions_20211112.py
@@ -0,0 +1,44 @@
+def initCard():
+    #   初始化步骤，程序开始时调用，并返回初始化的名片字典cards
+    cardlist = ()   # 初始化名片列表
+    printCardMenu()  # 打印菜单
+    return cardlist
+
+
+def printCardMenu():
+    #   打印菜单
+    print('----------------------------------')
+    print('--------名片管理系统 v1.0---------')
+    print('--------1.添加名片----------------')
+    print('--------2.删除名片----------------')
+    print('--------3.修改名片----------------')
+    print('--------4.查询名片----------------')
+    print('--------5.显示所有名片------------')
+    print('--------6.退出系统----------------')
+    print('------(输入数字回车选择功能)------')
+
+
+def addCard():
+    pass
+
+
+def deleteCard():
+    listCard()
+    pass
+
+
+def editCard():
+    pass
+
+
+def searchCard(searchVar):
+    #   按输入信息搜寻
+    pass
+
+
+def listCard():
+    pass
+
+
+def exitCard():
+    pass
